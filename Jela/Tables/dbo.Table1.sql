d14ed46f020c2a5bca45b11a8af0060e20f77f6b
katjushazzz <katjushazzz>
2021-01-19 16:33:54 +0100

    hj

--- Jela/Tables/dbo.Table1.sql
+++ Jela/Tables/dbo.Table1.sql
@@ -3,9 +3,8 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table1] (
-		[Test4]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[Test3]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table1] SET (LOCK_ESCALATION = TABLE)
 GO
-
