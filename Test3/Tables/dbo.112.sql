2f818d2ad0ee9aefd04a851b207a2e486bb53380
katjushazzz <katjushazzz>
2021-01-19 15:12:31 +0100

    Added Test column to 112 table

--- Test3/Tables/dbo.112.sql
+++ Test3/Tables/dbo.112.sql
@@ -4,7 +4,8 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[112] (
 		[Item]       [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[Status]     [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[Status]     [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[112] SET (LOCK_ESCALATION = TABLE)
