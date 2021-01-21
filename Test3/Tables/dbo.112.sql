4c012c10e3657eb364b79398bdb1d0a92133f414
katjushazzz <katjushazzz>
2021-01-21 15:12:28 +0100

    yuhij

--- Test3/Tables/dbo.112.sql
+++ Test3/Tables/dbo.112.sql
@@ -5,7 +5,8 @@ GO
 CREATE TABLE [dbo].[112] (
 		[Item]       [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Status]     [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[Test]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[Test]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test2]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[112] SET (LOCK_ESCALATION = TABLE)
