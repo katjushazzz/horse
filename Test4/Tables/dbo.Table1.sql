47bbff6d6f52ea68314e37e6168a8cc86b3e7f6f
katjushazzz <katjushazzz>
2021-01-22 15:13:09 +0100

    tyujk

--- Test4/Tables/dbo.Table1.sql
+++ Test4/Tables/dbo.Table1.sql
@@ -4,7 +4,9 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table1] (
 		[Test]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[Test2]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[Test2]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test3]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test4]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table1] SET (LOCK_ESCALATION = TABLE)
