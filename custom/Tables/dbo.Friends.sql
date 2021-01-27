e8cb568aeb4c3b3dfdf6e3526f9cd345e2725499
katjushazzz <katjushazzz>
2021-01-27 13:07:31 +0100

    wedf

--- custom/Tables/dbo.Friends.sql
+++ custom/Tables/dbo.Friends.sql
@@ -6,6 +6,7 @@ CREATE TABLE [dbo].[Friends] (
 		[FriendID]     [int] IDENTITY(1, 1) NOT NULL,
 		[Name]         [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 		[Surname]      [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Test]         [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [PK__Friends__A2CF6563A4A157C1]
 		PRIMARY KEY
 		CLUSTERED
