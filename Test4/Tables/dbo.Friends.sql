0f134ba420d32ab9f3b6130d679bb724aab44152
katjushazzz <katjushazzz>
2021-02-04 14:55:44 +0100

    gdfd

---
+++ Test4/Tables/dbo.Friends.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Friends] (
+		[FriendID]     [int] IDENTITY(1, 1) NOT NULL,
+		[Name]         [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+		[Surname]      [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		CONSTRAINT [PK__Friends__A2CF6563A4A157C1]
+		PRIMARY KEY
+		CLUSTERED
+		([FriendID])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Friends] SET (LOCK_ESCALATION = TABLE)
+GO
