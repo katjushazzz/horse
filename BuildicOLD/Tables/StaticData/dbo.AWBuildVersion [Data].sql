6d7b9653f50c5512b71432c0f2c2162f75f9767f
katjushazzz <katjushazzz>
2021-03-31 07:19:17 -0700

    Delete project

--- BuildicOLD/Tables/StaticData/dbo.AWBuildVersion [Data].sql
+++
@@ -1,4 +0,0 @@
-SET IDENTITY_INSERT [dbo].[AWBuildVersion] ON 
-
-INSERT [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (1, N'14.0.1000.169', CAST(N'2017-08-22T19:39:35.643' AS DateTime), CAST(N'2017-10-27T14:33:02.347' AS DateTime))
-SET IDENTITY_INSERT [dbo].[AWBuildVersion] OFF
