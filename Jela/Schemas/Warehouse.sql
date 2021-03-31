03ff75469fb0977c6a34c8459e57b7e903dd0832
katjushazzz <katjushazzz>
2021-03-31 07:18:16 -0700

    Delete project

--- Jela/Schemas/Warehouse.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA [Warehouse]
-	AUTHORIZATION [dbo]
-GO
-EXEC sp_addextendedproperty N'Description', N'Details of stock items, their holdings and transactions', 'SCHEMA', N'Warehouse', NULL, NULL, NULL, NULL
-GO
