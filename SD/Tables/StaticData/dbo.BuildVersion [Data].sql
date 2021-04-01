077bfaad7f76d4b03c35f0453910785924ffa228
katjushazzz <katjushazzz>
2021-04-01 07:27:58 -0700

    Delete project

--- SD/Tables/StaticData/dbo.BuildVersion [Data].sql
+++
@@ -1,3 +0,0 @@
-SET IDENTITY_INSERT [dbo].[BuildVersion] ON
-INSERT INTO [dbo].[BuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate], [Test12], [Test2]) VALUES (1, N'10.50.91013.00', '20091013 00:00:00.000', '20091013 00:00:00.000', N'edrfgh    ', NULL)
-SET IDENTITY_INSERT [dbo].[BuildVersion] OFF
--- SD/Views/SalesLT.vProductModelCatalogDescription.sql
+++
@@ -1,5 +0,0 @@
-SET ANSI_NULLS ON
-SET QUOTED_IDENTIFIER ON
-GO
---View [SalesLT].[vProductModelCatalogDescription] is encrypted. It could not be scripted
-GO
