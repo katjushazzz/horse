c8301d6728e9b8a5000646721aa18655115c4563
katjushazzz <katjushazzz>
2021-04-15 01:26:47 +0200

    sd

---
+++ Balls/Tables/StaticData/dbo.Customer [Data].sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Customer] ON
+INSERT INTO [dbo].[Customer] ([Customer_ID], [First_Name], [Last_Name], [Date_Of_Birth], [Montly_bill], [Email], [Credit_Card_Number], [Test], [Test2]) VALUES (1, N'kaca', N'kk', '19630309 13:16:06.000', 200, N'kaca@gmail.com', N'123-234', NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Customer] OFF
---
+++ Balls/Tables/StaticData/dbo.Employee [Data].sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Employee] ON
+INSERT INTO [dbo].[Employee] ([Emp_ID], [Emp_First_Name], [Emp_Last_Name], [Emp_Date_Of_Birth], [Emp_Salary], [Emp_Email], [Emp_Employment_Date]) VALUES (1, N'Gilbert', N'Smith', '19720630 00:00:00.000', 300, N'nuwxkd.idvzqpd@qng.com', '19630509 13:16:06.000')
+SET IDENTITY_INSERT [dbo].[Employee] OFF
