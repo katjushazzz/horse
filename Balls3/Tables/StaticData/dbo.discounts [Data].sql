6ac5f38369d472541876fddf837028319fe25d3c
katjushazzz <katjushazzz>
2021-04-19 14:12:18 +0200

    teh

--- Balls3/Tables/StaticData/dbo.discounts [Data].sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO [dbo].[discounts] ([discounttype], [stor_id], [lowqty], [highqty], [discount]) VALUES ('Customer Discount', '8042', NULL, NULL, CONVERT([decimal](4, 2),5.00))
-INSERT INTO [dbo].[discounts] ([discounttype], [stor_id], [lowqty], [highqty], [discount]) VALUES ('Initial Customer', NULL, NULL, NULL, CONVERT([decimal](4, 2),10.50))
-INSERT INTO [dbo].[discounts] ([discounttype], [stor_id], [lowqty], [highqty], [discount]) VALUES ('Volume Discount', NULL, 100, 1000, CONVERT([decimal](4, 2),6.70))
