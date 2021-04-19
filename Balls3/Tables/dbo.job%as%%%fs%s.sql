36f64ea53d8c26edc0778782a1f9bef7d75400df
katjushazzz <katjushazzz>
2021-04-19 15:31:59 +0200

    xsadf

--- Balls3/Tables/dbo.job%as%%%fs%s.sql
+++ Balls3/Tables/dbo.job%as%%%fs%s.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
-CREATE TABLE [dbo].[jobs] (
+CREATE TABLE [dbo].[job*/s] (
 		[job_id]       [smallint] IDENTITY(1, 1) NOT NULL,
 		[job_desc]     [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 		[min_lvl]      [tinyint] NOT NULL,
@@ -14,28 +14,28 @@ CREATE TABLE [dbo].[jobs] (
 	ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[jobs]
+ALTER TABLE [dbo].[job*/s]
 	ADD
 	CONSTRAINT [CK__jobs__min_lvl__534D60F1]
 	CHECK
 	([min_lvl]>=(10))
 GO
-ALTER TABLE [dbo].[jobs]
+ALTER TABLE [dbo].[job*/s]
 CHECK CONSTRAINT [CK__jobs__min_lvl__534D60F1]
 GO
-ALTER TABLE [dbo].[jobs]
+ALTER TABLE [dbo].[job*/s]
 	ADD
 	CONSTRAINT [CK__jobs__max_lvl__5441852A]
 	CHECK
 	([max_lvl]<=(250))
 GO
-ALTER TABLE [dbo].[jobs]
+ALTER TABLE [dbo].[job*/s]
 CHECK CONSTRAINT [CK__jobs__max_lvl__5441852A]
 GO
-ALTER TABLE [dbo].[jobs]
+ALTER TABLE [dbo].[job*/s]
 	ADD
 	CONSTRAINT [DF__jobs__job_desc__52593CB8]
 	DEFAULT ('New Position - title not formalized yet') FOR [job_desc]
 GO
-ALTER TABLE [dbo].[jobs] SET (LOCK_ESCALATION = TABLE)
+ALTER TABLE [dbo].[job*/s] SET (LOCK_ESCALATION = TABLE)
 GO
