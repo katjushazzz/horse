29fa21996965c516d4c7ae28fdd76c886d166cfa
katjushazzz <katjushazzz>
2021-04-20 10:11:38 +0200

    acv

--- Balls3/Tables/StaticData/dbo.publishers [Data].sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('0736', 'New Moon Books', 'Boston', 'MA', 'USA')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('0877', 'Binnet & Hardley', 'Washington', 'DC', 'USA')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('1389', 'Algodata Infosystems', 'Berkeley', 'CA', 'USA')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('1622', 'Five Lakes Publishing', 'Chicago', 'IL', 'USA')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('1756', 'Ramona Publishers', 'Dallas', 'TX', 'USA')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('9901', 'GGG&G', 'M?nchen', NULL, 'Germany')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('9952', 'Scootney Books', 'New York', 'NY', 'USA')
-INSERT INTO [dbo].[publishers] ([pub_id], [pub_name], [city], [state], [country]) VALUES ('9999', 'Lucerne Publishing', 'Paris', NULL, 'France')
